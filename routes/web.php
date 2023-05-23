0cbc67c723eb3859883245de87d112eb664cd427
chuites <carlosehs.9319@gmail.com>
2023-05-22 19:19:28 -0600

    Test APP URL

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,6 @@ Route::get('/', function () {
 });
 Route::post('login', 'App\Http\Controllers\ApiController@login')->name("login");
 
-/* Route::middleware([CheckApiToken::class])->group(function () {
-    Route::get('welcome', 'App\Http\Controllers\ApiController@welcome')->name("welcome");
-    Route::post('TestApi', 'App\Http\Controllers\ApiController@testapi')->name("TestApi");
-    Route::post('crearCuenta', 'App\Http\Controllers\ApiController@crearCuenta')->name("crearCuenta");
-    Route::post('testTransporte', 'App\Http\Controllers\ApiController@testTransporte')->name("testTransporte");
-    Route::post('testPiloto', 'App\Http\Controllers\ApiController@testPiloto')->name("testPiloto");
-    Route::post('enviarCargamento', 'App\Http\Controllers\ApiController@enviarCargamento')->name("enviarCargamento");
-}); */
 Route::get('welcome', 'App\Http\Controllers\ApiController@welcome')->name("welcome");
 Route::post('TestApi', 'App\Http\Controllers\ApiController@testapi')->name("TestApi");
 Route::post('logout', 'App\Http\Controllers\ApiController@logout')->name("logout");
